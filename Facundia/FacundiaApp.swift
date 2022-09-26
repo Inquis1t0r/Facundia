c451f4717a09a01d14921986de289b6c2442512c
inquis1t0r <pi0tr3k9292@gmail.com>
2022-09-26 09:41:21 +0200

    Initial Commit

---
+++ Facundia/FacundiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacundiaApp.swift
+//  Facundia
+//
+//  Created by Piotr Kazimierski on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct FacundiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FacundiaUITests/FacundiaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FacundiaUITestsLaunchTests.swift
+//  FacundiaUITests
+//
+//  Created by Piotr Kazimierski on 26/09/2022.
+//
+
+import XCTest
+
+final class FacundiaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
